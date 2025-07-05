1171d3ede4a8590854216dd61374b70b22e4ad5b
Shehriyar Qureshi <thatdevsherry@gmail.com>
2025-07-05 12:33:41 +0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use color_eyre::Result;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use ratatui::{layout::Rect, widgets::Paragraph, DefaultTerminal, Frame};
+use suzui_rs::sdl::SuzukiSdlViewer;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = App::new().run(terminal);
+    ratatui::restore();
+    result
+}
+
+/// The main application which holds the state and logic of the application.
+#[derive(Debug, Default)]
+pub struct App {
+    /// Is the application running?
+    running: bool,
+    sdl_viewer: SuzukiSdlViewer,
+}
+
+impl App {
+    /// Construct a new instance of [`App`].
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Run the application's main loop.
+    pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        self.running = true;
+        self.sdl_viewer.connect();
+        while self.running {
+            self.sdl_viewer.update_raw_data();
+            terminal.draw(|frame| self.render(frame))?;
+            self.handle_crossterm_events()?;
+        }
+        Ok(())
+    }
+
+    /// Renders the user interface.
+    ///
+    /// This is where you add new widgets. See the following resources for more information:
+    ///
+    /// - <https://docs.rs/ratatui/latest/ratatui/widgets/index.html>
+    /// - <https://github.com/ratatui/ratatui/tree/main/ratatui-widgets/examples>
+    fn render(&mut self, frame: &mut Frame) {
+        for (idx, (addr, value)) in self.sdl_viewer.raw_data.iter().enumerate() {
+            let area = Rect::new(0, idx.try_into().unwrap(), 10, 1);
+            let area_value = Rect::new(3, (idx + 1).try_into().unwrap(), 10, 1);
+            frame.render_widget(Paragraph::new(format!("{}:", addr)), area);
+            frame.render_widget(Paragraph::new(value.to_string()), area_value);
+        }
+    }
+
+    /// Reads the crossterm events and updates the state of [`App`].
+    ///
+    /// If your application needs to perform work in between handling events, you can use the
+    /// [`event::poll`] function to check if there are any events available with a timeout.
+    fn handle_crossterm_events(&mut self) -> Result<()> {
+        match event::read()? {
+            // it's important to check KeyEventKind::Press to avoid handling key release events
+            Event::Key(key) if key.kind == KeyEventKind::Press => self.on_key_event(key),
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+            _ => {}
+        }
+        Ok(())
+    }
+
+    /// Handles the key events and updates the state of [`App`].
+    fn on_key_event(&mut self, key: KeyEvent) {
+        match (key.modifiers, key.code) {
+            (_, KeyCode::Esc | KeyCode::Char('q'))
+            | (KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => self.quit(),
+            // Add other key handlers here.
+            _ => {}
+        }
+    }
+
+    /// Set running to false to quit the application.
+    fn quit(&mut self) {
+        self.running = false;
+    }
+}
---
+++ src/sdl.rs
@@ -0,0 +1,400 @@
+use serialport::TTYPort;
+use std::{
+    collections::HashMap,
+    io::{Read, Write},
+    time::Duration,
+};
+use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumIter, FromRepr};
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, EnumIter, Hash, FromRepr, Display)]
+pub enum ObdAddress {
+    FaultCodes1 = 0x00,
+    FaultCodes2 = 0x01,
+    FaultCodes3 = 0x02,
+    FaultCodes4 = 0x03,
+    RpmHigh = 0x04,
+    RpmLow = 0x05,
+    TargetIdle = 0x06,
+    VehicleSpeedSensor = 0x07,
+    EngineCoolantTemperature = 0x08,
+    IntakeAirTemperature = 0x09,
+    TpsAngle = 0x0A,
+    TpsVoltage = 0x0B,
+    InjectorPulseWidthHigh = 0x0D,
+    InjectorPulseWidthLow = 0x0E,
+    IgnitionAdvance = 0x0F,
+    ManifoldAbsolutePressure = 0x10,
+    BarometricPressure = 0x11,
+    IscFlowDuty = 0x12,
+    MixControlDwell = 0x13,
+    MixControlLearning = 0x14,
+    MixControlMonitor = 0x15,
+    BatteryVoltage = 0x16,
+    RadiatorFan = 0x19,
+    StatusFlags1 = 0x1a,
+    FaultCodes5 = 0x20,
+    FaultCodes6 = 0x21,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, EnumIter, Hash)]
+pub enum ScanToolParameter {
+    DesiredIdle,
+    EngineSpeed,
+    IacFlowDutyCycle,
+    AbsoluteThrottlePosition,
+    InjPulseWidthCyl1,
+    CoolantTemp,
+    VehicleSpeed,
+    IntakeAirTemp,
+    Map,
+    BarometricPressure,
+    TpSensorVolt,
+    BatteryVoltage,
+    IgnitionAdvance,
+    ClosedThrottlePos,
+    ElectricLoad,
+    FuelCut,
+    AcSwitch,
+    PspSwitch,
+    RadiatorFan,
+}
+
+enum Actuate {
+    None = 0x00,
+    FixedSpark = 0x10,
+    Isc = 0xc0,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, FromRepr)]
+enum SdlHeader {
+    EcuId = 0x10,
+    EcuData = 0x13,
+    EcuActuate = 0x15,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct SdlMessage {
+    header: SdlHeader,
+    length: u8,
+    data: Option<Vec<u8>>,
+    checksum: u8,
+}
+
+impl SdlMessage {
+    fn generate_checksum(bytes: &[u8]) -> u8 {
+        bytes
+            .iter()
+            .fold(0u8, |acc, &b| acc.wrapping_add(b))
+            .wrapping_neg()
+    }
+
+    pub fn new(header: SdlHeader, data: Option<Vec<u8>>) -> Self {
+        let data = data.unwrap_or_default();
+        let length = header as u8 + data.len() as u8 + 1;
+        let checksum = Self::generate_checksum(&[&[header as u8, length], &data[..]].concat());
+        Self {
+            header,
+            length,
+            data: Some(data),
+            checksum,
+        }
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes: Vec<u8> = Vec::new();
+        bytes.push(self.header as u8);
+        bytes.push(self.length);
+        if let Some(data) = &self.data {
+            let data_bytes: Vec<u8> = data.iter().map(|&v| v as u8).collect();
+            bytes.extend_from_slice(&data_bytes);
+        }
+        bytes.push(self.checksum);
+        bytes
+    }
+}
+
+impl TryFrom<&[u8]> for SdlMessage {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> std::result::Result<Self, Self::Error> {
+        if value.len() < 3 {
+            return Err("length of bytes less than min. of 3".to_string());
+        }
+        let header = value[0];
+        let header_enum = SdlHeader::from_repr(header.into()).unwrap();
+        let length = value[1];
+        let data = &value[2..length as usize - 1];
+        let d = if data.len() > 0 {
+            data.to_owned().into_iter().collect()
+        } else {
+            vec![]
+        };
+        let checksum = value[length as usize - 1];
+        Ok(Self {
+            header: header_enum,
+            length,
+            data: Some(d),
+            checksum,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct SuzukiSdlViewer {
+    port: TTYPort,
+    pub raw_data: HashMap<ObdAddress, u8>,
+    scan_tool_data: HashMap<ScanToolParameter, String>,
+}
+
+impl Default for SuzukiSdlViewer {
+    fn default() -> Self {
+        let vag_kkl = serialport::new("/dev/ttyUSB0", 7812)
+            .timeout(Duration::from_secs(1))
+            .open_native()
+            .expect("Failed to open port");
+        let mut scan_tool_data: HashMap<ScanToolParameter, String> = HashMap::new();
+        let mut raw_data: HashMap<ObdAddress, u8> = HashMap::new();
+
+        for obd_address in ObdAddress::iter() {
+            raw_data.insert(obd_address, 0);
+        }
+        for scan_tool_parameter in ScanToolParameter::iter() {
+            scan_tool_data.insert(scan_tool_parameter, "N/A".to_string());
+        }
+        Self {
+            port: vag_kkl,
+            scan_tool_data: HashMap::new(),
+            raw_data: HashMap::new(),
+        }
+    }
+}
+
+impl SuzukiSdlViewer {
+    /// Query ECU ID.
+    fn get_ecu_id(&mut self) -> String {
+        let header = SdlHeader::EcuId;
+        let data = None;
+        let sdl_message = SdlMessage::new(header, data);
+        let written = self.port.write(&sdl_message.to_bytes());
+        println!("wrote {:?} bytes", written);
+        let bytes_written = written.unwrap();
+        let mut response_buf = vec![0; bytes_written];
+        let bytes_read_echo = self.port.read(response_buf.as_mut_slice()); // echo
+        println!("read echo: {:?} bytes", bytes_read_echo);
+        let bytes_read = self.port.read(response_buf.as_mut_slice());
+        println!("read response: {:?} bytes", bytes_read);
+        "FFFF".to_string()
+    }
+
+    /// Query obd addresses and update raw data.
+    pub fn update_raw_data(&mut self) {
+        let header = SdlHeader::EcuData;
+        let data = Some(ObdAddress::iter().map(|v| v as u8).collect());
+        let sdl_message = SdlMessage::new(header, data);
+        let written = self.port.write(&sdl_message.to_bytes());
+        println!("wrote {:?} bytes", written);
+        let bytes_written = written.unwrap();
+        let mut response_buf = vec![0; bytes_written];
+        let bytes_read_echo = self.port.read(response_buf.as_mut_slice()); // echo
+        println!("read echo: {:?} bytes", bytes_read_echo);
+        let bytes_read = self.port.read(response_buf.as_mut_slice());
+        println!("read response: {:?} bytes", bytes_read);
+
+        let request = sdl_message;
+        let response = SdlMessage::try_from(&response_buf[..]).unwrap();
+
+        if let Some(addrs) = request.data {
+            if let Some(values) = response.data {
+                for (addr, value) in addrs.iter().zip(values.iter()) {
+                    if let Some(obd_addr) = ObdAddress::from_repr(*addr as usize) {
+                        self.raw_data.insert(obd_addr, *value);
+                    }
+                }
+            }
+        }
+    }
+
+    /// Update scan tool data from raw values.
+    pub fn update_processed_data(&mut self) {
+        for scan_tool_parameter in ScanToolParameter::iter() {
+            match scan_tool_parameter {
+                ScanToolParameter::DesiredIdle => {
+                    let raw_value = self.raw_data.get(&ObdAddress::TargetIdle).unwrap();
+                    let processed_value = *raw_value as f32 * 7.84375;
+                    self.scan_tool_data.insert(
+                        scan_tool_parameter,
+                        format!("{} RPM", processed_value.round()),
+                    );
+                }
+                ScanToolParameter::EngineSpeed => {
+                    let low_byte = self.raw_data.get(&ObdAddress::RpmLow).unwrap();
+                    let high_byte = self.raw_data.get(&ObdAddress::RpmHigh).unwrap();
+                    let processed_value = ((high_byte << 8) | low_byte) as f32 / 5.1;
+                    self.scan_tool_data.insert(
+                        scan_tool_parameter,
+                        format!("{} RPM", processed_value.round()),
+                    );
+                }
+                ScanToolParameter::IacFlowDutyCycle => {
+                    let raw_value = self.raw_data.get(&ObdAddress::IscFlowDuty).unwrap();
+                    let processed_value = (*raw_value as f32 / 255.0) * 100.0;
+                    self.scan_tool_data.insert(
+                        scan_tool_parameter,
+                        format!("{} %", processed_value.round()),
+                    );
+                }
+                ScanToolParameter::TpSensorVolt => {
+                    let raw_value = self.raw_data.get(&ObdAddress::TpsVoltage).unwrap();
+                    let processed_value = (*raw_value as f32 / 255.0) * 5.0;
+                    self.scan_tool_data.insert(
+                        scan_tool_parameter,
+                        format!("{} V", processed_value.round()),
+                    );
+                }
+                ScanToolParameter::BatteryVoltage => {
+                    let raw_value = self.raw_data.get(&ObdAddress::BatteryVoltage).unwrap();
+                    let processed_value = *raw_value as f32 * 0.0787;
+                    self.scan_tool_data.insert(
+                        scan_tool_parameter,
+                        format!("{} V", processed_value.round()),
+                    );
+                }
+                ScanToolParameter::CoolantTemp | ScanToolParameter::IntakeAirTemp => {
+                    let raw_value = if scan_tool_parameter == ScanToolParameter::CoolantTemp {
+                        self.raw_data
+                            .get(&ObdAddress::EngineCoolantTemperature)
+                            .unwrap()
+                    } else {
+                        self.raw_data
+                            .get(&ObdAddress::IntakeAirTemperature)
+                            .unwrap()
+                    };
+                    let processed_value = (*raw_value as f32 / 255.0) * 159.0 - 40.0;
+                    self.scan_tool_data.insert(
+                        scan_tool_parameter,
+                        format!("{} C", processed_value.round()),
+                    );
+                }
+                ScanToolParameter::InjPulseWidthCyl1 => {
+                    let low_byte = self
+                        .raw_data
+                        .get(&ObdAddress::InjectorPulseWidthLow)
+                        .unwrap();
+                    let high_byte = self
+                        .raw_data
+                        .get(&ObdAddress::InjectorPulseWidthHigh)
+                        .unwrap();
+                    let processed_value = ((high_byte << 8) | low_byte) as f32 * 0.002;
+                    self.scan_tool_data
+                        .insert(scan_tool_parameter, format!("{:.2} ms", processed_value));
+                }
+                ScanToolParameter::Map | ScanToolParameter::BarometricPressure => {
+                    let raw_value = if scan_tool_parameter == ScanToolParameter::Map {
+                        self.raw_data
+                            .get(&ObdAddress::ManifoldAbsolutePressure)
+                            .unwrap()
+                    } else {
+                        self.raw_data.get(&ObdAddress::BarometricPressure).unwrap()
+                    };
+                    let processed_value =
+                        (*raw_value as f32 / 255.0) * (146.63 - (-20.0)) + (-20.0);
+                    self.scan_tool_data
+                        .insert(scan_tool_parameter, format!("{:.1} kPa", processed_value));
+                }
+                ScanToolParameter::AbsoluteThrottlePosition => {
+                    let raw_value = self.raw_data.get(&ObdAddress::TpsVoltage).unwrap();
+                    let processed_value = (*raw_value as f32 / 255.0) * 100.0;
+                    let abs_throttle_pos =
+                        self.scan_tool_data.get_mut(&scan_tool_parameter).unwrap();
+                    *abs_throttle_pos = format!("{} %", processed_value.round());
+                    self.scan_tool_data
+                        .insert(scan_tool_parameter, format!("{} %", processed_value));
+                }
+                ScanToolParameter::VehicleSpeed => {
+                    let raw_value = self.raw_data.get(&ObdAddress::VehicleSpeedSensor).unwrap();
+                    let processed_value = *raw_value;
+                    self.scan_tool_data
+                        .insert(scan_tool_parameter, format!("{} km/h", processed_value));
+                }
+                ScanToolParameter::IgnitionAdvance => {
+                    let raw_value = self.raw_data.get(&ObdAddress::IgnitionAdvance).unwrap();
+                    let processed_value = (*raw_value as f32 / 255.0) * (78.0 - (-12.0)) + (-12.0);
+                    self.scan_tool_data.insert(
+                        scan_tool_parameter,
+                        format!("{} BTDC", processed_value.round()),
+                    );
+                }
+                ScanToolParameter::PspSwitch => {
+                    let raw_value = self.raw_data.get(&ObdAddress::StatusFlags1).unwrap();
+                    let processed_value = if raw_value & (1 << 1) != 0 {
+                        "ON"
+                    } else {
+                        "OFF"
+                    };
+                    self.scan_tool_data
+                        .insert(scan_tool_parameter, processed_value.to_string());
+                }
+                ScanToolParameter::AcSwitch => {
+                    let raw_value = self.raw_data.get(&ObdAddress::StatusFlags1).unwrap();
+                    let processed_value = if raw_value & (1 << 2) != 0 {
+                        "ON"
+                    } else {
+                        "OFF"
+                    };
+                    self.scan_tool_data
+                        .insert(scan_tool_parameter, processed_value.to_string());
+                }
+                ScanToolParameter::ClosedThrottlePos => {
+                    let raw_value = self.raw_data.get(&ObdAddress::StatusFlags1).unwrap();
+                    let processed_value = if raw_value & (1 << 4) != 0 {
+                        "ON"
+                    } else {
+                        "OFF"
+                    };
+                    self.scan_tool_data
+                        .insert(scan_tool_parameter, processed_value.to_string());
+                }
+                ScanToolParameter::ElectricLoad => {
+                    let raw_value = self.raw_data.get(&ObdAddress::StatusFlags1).unwrap();
+                    let processed_value = if raw_value & (1 << 6) != 0 {
+                        "ON"
+                    } else {
+                        "OFF"
+                    };
+                    self.scan_tool_data
+                        .insert(scan_tool_parameter, processed_value.to_string());
+                }
+                ScanToolParameter::RadiatorFan => {
+                    let raw_value = self.raw_data.get(&ObdAddress::RadiatorFan).unwrap();
+                    let processed_value = if *raw_value == 128 { "ON" } else { "OFF" };
+                    self.scan_tool_data
+                        .insert(ScanToolParameter::RadiatorFan, processed_value.to_string());
+                }
+                ScanToolParameter::FuelCut => {
+                    let low_byte = self
+                        .raw_data
+                        .get(&ObdAddress::InjectorPulseWidthLow)
+                        .unwrap();
+                    let high_byte = self
+                        .raw_data
+                        .get(&ObdAddress::InjectorPulseWidthHigh)
+                        .unwrap();
+                    let processed_value = if *low_byte == 0 && *high_byte == 0 {
+                        "ON"
+                    } else {
+                        "OFF"
+                    };
+                    self.scan_tool_data
+                        .insert(ScanToolParameter::FuelCut, processed_value.to_string());
+                }
+            }
+        }
+    }
+
+    /// Send ID request to ECU as a means of verifying connection.
+    pub fn connect(&mut self) {
+        let ecu_id = self.get_ecu_id();
+        println!("ecu_id: {}", ecu_id);
+    }
+}
