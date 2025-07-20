f78ffea91236965163f4d92552218d2cb01b5dec
Shehriyar Qureshi <thatdevsherry@gmail.com>
2025-07-21 00:01:26 +0500

    fix: change distance_fuel file path
    less complexity, funny place tho

--- src/strings.rs
+++ src/strings.rs
@@ -1,5 +1,9 @@
+// you will LOL at the path but it's the simplest solution.
+// I've set root as read-only and instead of trying to setup a certain dir
+// as writeable, saving to `/boot` is easiest as that is on another
+// partition and is still writeable even if "ro" flag is set.
 #[cfg(target_arch = "aarch64")]
-pub const DISTANCE_FUEL_FILE_PATH: &str = "/home/dietpi/distance_fuel";
+pub const DISTANCE_FUEL_FILE_PATH: &str = "/boot/distance_fuel";
 
 #[cfg(not(target_arch = "aarch64"))]
 pub const DISTANCE_FUEL_FILE_PATH: &str = "/tmp/distance_fuel";
