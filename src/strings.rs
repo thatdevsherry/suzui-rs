ca016aa20dc1f143a602ea172524fb26e79dc64f
Shehriyar Qureshi <thatdevsherry@gmail.com>
2025-07-21 17:44:01 +0500

    chore: ttyUSB0 as string

--- src/strings.rs
+++ src/strings.rs
@@ -7,3 +7,5 @@ pub const DISTANCE_FUEL_FILE_PATH: &str = "/boot/firmware/distance_fuel";
 
 #[cfg(not(target_arch = "aarch64"))]
 pub const DISTANCE_FUEL_FILE_PATH: &str = "/tmp/distance_fuel";
+
+pub const VAG_KKL_PORT: &str = "/dev/ttyUSB0";
