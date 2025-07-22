6412a1402b0e8cb3bc3990fcc608462338d76b16
Shehriyar Qureshi <thatdevsherry@gmail.com>
2025-07-22 20:21:28 +0500

    fix(toggle)!: set initial state
    we don't have engine context when initializing toggle detector
    
    fixing by using an option and setting to Some(_) in the first frame
    render

--- src/toggle_detector.rs
+++ src/toggle_detector.rs
@@ -2,7 +2,7 @@ use std::time::{Duration, Instant};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ToggleDetector {
-    last_state: bool,
+    last_state: Option<bool>,
     toggle_count: u8,
     first_toggle_time: Option<Instant>,
     required_toggles: u8,
@@ -12,16 +12,22 @@ pub struct ToggleDetector {
 impl ToggleDetector {
     pub fn new() -> Self {
         Self {
-            last_state: false,
+            last_state: None,
             toggle_count: 0,
             first_toggle_time: None,
-            required_toggles: 6,
-            time_window: Duration::from_secs(8),
+            required_toggles: 5,
+            time_window: Duration::from_secs(10),
         }
     }
 
     pub fn update(&mut self, current_el_state: bool) -> bool {
-        if current_el_state != self.last_state {
+        // setup initializer value
+        if self.last_state.is_none() {
+            self.last_state = Some(current_el_state);
+            return false;
+        }
+
+        if current_el_state != self.last_state.unwrap() {
             let now = Instant::now();
 
             if self.first_toggle_time.is_none() {
@@ -29,20 +35,22 @@ impl ToggleDetector {
                 self.toggle_count = 1;
             } else if now.duration_since(self.first_toggle_time.unwrap()) <= self.time_window {
                 self.toggle_count += 1;
-
                 if self.toggle_count >= self.required_toggles {
                     self.reset_detector();
                     return true;
                 }
             } else {
+                // Window expired - start completely fresh
+                self.reset_detector();
                 self.first_toggle_time = Some(now);
-                self.toggle_count = 1;
+                self.toggle_count = 0;
             }
         } else if let Some(first_time) = self.first_toggle_time {
             if Instant::now().duration_since(first_time) > self.time_window {
                 self.reset_detector();
             }
         }
+        self.last_state = Some(current_el_state);
         false
     }
 
