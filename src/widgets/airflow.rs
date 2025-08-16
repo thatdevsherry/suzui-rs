1962d3567115b1a559d37779cd5b8d56574f18f7
Shehriyar Qureshi <thatdevsherry@gmail.com>
2025-08-16 15:31:03 +0500

    feat: red color load > 85 and < 2000 rpm

--- src/widgets/airflow.rs
+++ src/widgets/airflow.rs
@@ -9,6 +9,7 @@ pub struct AirflowBlock {
     map: f32,
     baro: f32,
     calc_load: u8,
+    rpm: u16,
 }
 
 impl AirflowBlock {
@@ -17,6 +18,7 @@ impl AirflowBlock {
             map: ctx.manifold_absolute_pressure,
             baro: ctx.barometric_pressure,
             calc_load: ctx.calculated_load,
+            rpm: ctx.engine_speed,
         }
     }
 }
@@ -52,7 +54,8 @@ impl Widget for AirflowBlock {
                 .add_modifier(Modifier::BOLD),
         )
         .render(airflow_layout[1], buf);
-        let gauge_color = Color::White;
+
+        // doing this as load can go over 100% in certain cases.
         let calc_load_min = 0;
         let calc_load_max = 100;
         let calc_load_percentage = if self.calc_load <= calc_load_min {
@@ -62,6 +65,10 @@ impl Widget for AirflowBlock {
         } else {
             ((self.calc_load - calc_load_min) as f32 / (calc_load_max - calc_load_min) as f32
                 * 100.0) as u16
+        };
+        let gauge_color = match self.rpm {
+            rpm if rpm < 2000 && self.calc_load >= 85 => Color::Red,
+            _ => Color::White,
         };
         Gauge::default()
             .percent(calc_load_percentage)
